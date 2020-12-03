214da820ef359daff2b7b4dae3f4ac7c2aaa487d
Vijai <Vijai@DESKTOP-CDKFQKQ>
2020-12-04 01:05:04 +0530

    My first commit

---
+++ Hello/src/sales/products/Test.java
@@ -0,0 +1,16 @@
+package sales.products;
+
+class Test
+{
+public static void main(String[] args)
+{
+	String str1 = "Java";
+	String str2 = new String("java");
+	if(str1.toLowerCase()==str2.toLowerCase())
+	{
+		System.out.println("true");
+	}
+	
+
+}
+}
